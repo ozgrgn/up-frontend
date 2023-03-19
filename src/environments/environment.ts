63c556e05a22f26dad9e50860446140bef8a0aa7
Oz <ozgrgn@gmail.com>
2023-03-19 16:29:07 +0300

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
- apiUrl: 'http://localhost:3000',
-production: false,
+    apiUrl: 'https://up-backend-d6ydevoyqq-ew.a.run.app',
+    production: false,
 };
 
 //WTPBxiTdWQhNiCf2
